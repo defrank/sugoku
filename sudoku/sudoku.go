e24e36452a0786a43d95ff9b02d36d98a870db05
Derek Michael Frank <derek@fanai.io>
2020-02-01 20:48:11 -0800

    chore(grid): Add bounds checks

--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -1,17 +1,108 @@
 package sudoku
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type cell struct {
 	value    int
 	possible [9]bool
 }
 
-func (c cell) Set(value int) {
-	c.value = value
+func (c *cell) get() int {
+	return c.value
+}
+
+func (c *cell) set(v int) {
+	if v < 1 || 9 < v {
+		panic(errors.New("value must be between 1 and 9"))
+	}
+	c.value = v
+}
+
+func (c *cell) notes() (n []int) {
+	for i, p := range c.possible {
+		if p {
+			n = append(n, i+1)
+		}
+	}
+	return
+}
+
+func (c *cell) note(value int) {
+	c.possible[value] = true
+}
+
+func (c *cell) unnote(value int) {
+	c.possible[value] = false
+}
+
+func (c *cell) String() string {
+	if value := c.value; value > 0 {
+		return fmt.Sprintf("%d", value)
+	}
+	notes := c.notes()
+	var joinable []string
+	for _, n := range notes {
+		joinable = append(joinable, fmt.Sprintf("%d", n))
+	}
+	return "{" + strings.Join(joinable, ",") + "}"
 }
 
 type Segment struct {
 }
 
 type Grid struct {
-	values [9][9]cell
+	size  int
+	cells [][]cell
+}
+
+func NewGrid(n int) (g *Grid) {
+	if n < 2 {
+		panic(errors.New("size must be larger than 2"))
+	}
+
+	cells := make([][]cell, n, n)
+	for j := range cells {
+		cells[j] = make([]cell, n, n)
+		for i := 0; i < n; i++ {
+			cells[j][i] = cell{}
+		}
+	}
+
+	g = &Grid{size: n, cells: cells}
+	return
+}
+
+func (g *Grid) Get(x, y int) (v int) {
+	g.indexBoundsCheck(x, "x")
+	g.indexBoundsCheck(y, "y")
+
+	v = g.cells[y][x].value
+	g.valueBoundsCheck(v)
+	return
+}
+
+func (g *Grid) Set(x, y, v int) {
+	g.indexBoundsCheck(x, "x")
+	g.indexBoundsCheck(y, "y")
+	g.valueBoundsCheck(v)
+
+	g.cells[y][x].value = v
+}
+
+func (g *Grid) indexBoundsCheck(i int, s string) {
+	if i < 0 || i > g.size-1 {
+		msg := fmt.Sprintf("%q must be between 0 and %d", s, g.size-1)
+		panic(errors.New(msg))
+	}
+}
+
+func (g *Grid) valueBoundsCheck(v int) {
+	if v <= 0 || v > g.size {
+		msg := fmt.Sprintf("%q must be between 1 and %d", "value", g.size)
+		panic(errors.New(msg))
+	}
 }
--- sudoku/sudoku_test.go
+++ sudoku/sudoku_test.go
@@ -2,4 +2,117 @@ package sudoku
 
 import "testing"
 
-func Test
+func TestNewCellGet(t *testing.T) {
+	c := &cell{}
+	want := 0
+	if got := c.get(); got != want {
+		t.Errorf("&cell{}.get() == %d, expected %d", got, want)
+	}
+}
+
+func TestCellSetInBounds(t *testing.T) {
+	c := &cell{}
+	want := 1
+	c.set(want)
+	if got := c.get(); got != want {
+		t.Errorf("cell.set(1) == %d, expected %d", got, want)
+	}
+}
+
+func TestCellSetOutOfBounds(t *testing.T) {
+	c := &cell{}
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Did not panic on out of bounds value")
+		}
+	}()
+
+	c.set(0)
+}
+
+func TestNewCellNotes(t *testing.T) {
+	c := &cell{}
+	if got := c.notes(); got != nil {
+		t.Errorf("&cell{}.notes() == %d, expected nil", got)
+	}
+}
+
+func TestNewCellString(t *testing.T) {
+	c := &cell{}
+	want := "{}"
+	if got := c.String(); got != want {
+		t.Errorf("NewCell().String() == %q, expected %q", got, want)
+	}
+}
+
+func TestNewGridCells(t *testing.T) {
+	n := 9
+	g := NewGrid(n)
+	if height := len(g.cells); height != n {
+		t.Errorf("Grid height == %d, expected %d", height, n)
+	}
+	if capacity := cap(g.cells); capacity != n {
+		t.Errorf("Grid capacity == %d, expected %d", capacity, n)
+	}
+	for _, row := range g.cells {
+		if width := len(row); width != n {
+			t.Errorf("Grid width == %d, expected %d", width, n)
+		}
+		if capacity := cap(row); capacity != n {
+			t.Errorf("Grid row capacity == %d, expected %d", capacity, n)
+		}
+		for _, cell := range row {
+			if value := cell.get(); value != 0 {
+				t.Errorf("Cell value == %d, expected 0", value)
+			}
+			if notes := cell.notes(); notes != nil {
+				t.Errorf("Cell notes == %d, expected nil", notes)
+			}
+		}
+	}
+}
+
+func TestGridSetValueOutOfBounds(t *testing.T) {
+	n := 9
+	g := NewGrid(n)
+	v := n + 1
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Did not panic on out of bounds value")
+		}
+	}()
+
+	g.Set(0, 0, v)
+}
+
+func TestGridSetIndexColumnOutOfBounds(t *testing.T) {
+
+	test := func(n, c, r int) {
+		g := NewGrid(n)
+
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("Did not panic on out of bounds index")
+			}
+		}()
+
+		g.Set(c, r, n)
+	}
+
+	for n := 3; n < 100; n *= n {
+		for i := -100; i < 0; i += 10 {
+			test(n, i, 0)
+			test(n, i, n-1)
+			test(n, 0, i)
+			test(n, n-1, i)
+		}
+		for i := n; i < 100; i += 10 {
+			test(n, i, 0)
+			test(n, i, n-1)
+			test(n, 0, i)
+			test(n, n-1, i)
+		}
+	}
+}
